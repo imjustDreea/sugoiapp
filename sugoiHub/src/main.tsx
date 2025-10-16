f8e25364ae8d5c61603d641d6fd5fba0ff6e0eef
andreea <andaandrea231@gmail.com>
2025-10-16 18:09:20 +0200

    cambios to be committed:

--- sugoiHub/src/main.tsx
+++ sugoiHub/src/main.tsx
@@ -1,10 +1,10 @@
-
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
 
-
-
-const root = createRoot(document.getElementById('root'))
-  root.render("Hello World")
-
-
-
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
