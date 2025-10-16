040bc8752d740e338ac9e53aba38739911c63bce
andreea <andaandrea231@gmail.com>
2025-10-16 17:58:37 +0200

    Nuevo

---
+++ sugoiHub/src/main.tsx
@@ -0,0 +1,10 @@
+
+import { createRoot } from 'react-dom/client'
+
+
+
+const root = createRoot(document.getElementById('root'))
+  root.render("Hello World")
+
+
+
--- sugoiHubApp/src/components/ui/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as Button } from './Button';
-export { default as Card } from './Card';
-// form components
-export { default as Input } from '../form/Input';
-
