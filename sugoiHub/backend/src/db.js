04781255a45bab9b838e300aa27af6b52e20038b
andreea <andaandrea231@gmail.com>
2025-11-25 10:45:33 +0100

    borrado backend

--- sugoiHub/backend/src/db.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// Use require to avoid TypeScript needing external declaration files here
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const Database = require("better-sqlite3");
-const db = new Database("database.sqlite");
-exports.default = db;
-//# sourceMappingURL=db.js.map
--- sugoiHub/backend/src/db.ts
+++
@@ -1,7 +0,0 @@
-// Use require to avoid TypeScript needing external declaration files here
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const Database: any = require("better-sqlite3");
-
-const db = new Database("database.sqlite");
-
-export default db;
